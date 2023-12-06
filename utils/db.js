3e46618963c9959ed3557717ce206052e626f3b1
Epicsociety <measkswho@gmail.com>
2023-12-06 18:45:21 +0300

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor () {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB connection error:', err);
+      } else {
+        console.log('Connected to MongoDB');
+      }
+    });
+  }
+
+  isAlive () {
+    return this.client.isConnected();
+  }
+
+  async nbUsers () {
+    const usersCollection = this.client.db().collection('users');
+    const count = await usersCollection.countDocuments();
+    return count;
+  }
+
+  async nbFiles () {
+    const filesCollection = this.client.db().collection('files');
+    const count = await filesCollection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
