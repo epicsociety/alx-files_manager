dc996eed956af346eaed7943e8a58a02a383bf96
Roro1839 <ronaldokipkirui90@gmail.com>
2023-12-08 00:12:09 +0300

    update

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,6 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
-
+import redisClient from '../utils/redis';
 // const { ObjectId } = require('mongodb');
 
 class UsersController {
@@ -35,6 +35,21 @@ class UsersController {
     };
 
     return res.status(201).json(createdUser);
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token') || null;
+    if (!token) return response.status(401).send({ error: 'Unauthorized' });
+
+    const redisToken = await redisClient.get(`auth_${token}`);
+    if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
+
+    // eslint-disable-next-line no-undef
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    delete user.password;
+
+    return response.status(200).send({ id: user._id, email: user.email });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
 
 router.get('/status', AppController.getStatus);
 
