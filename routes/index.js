535c302fef7d349be4729f9910272874aaa5af9c
Roro1839 <ronaldokipkirui90@gmail.com>
2023-12-09 04:41:48 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 router.get('/connect', AuthController.getConnect);
 
